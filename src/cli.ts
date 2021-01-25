e9a5399266e4fef549f0a2f3db7d284395ebd38d
P-Chan <hello@0x50.io>
2021-01-25 09:42:55 +0900

    feat: Improve cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,12 @@
 import { cac } from 'cac'
 import { validate } from './validate'
+import consola from 'consola'
 
 const cli = cac()
 
 cli
   .command('[...src]', 'Validate files')
-  .option('-s [url], --schema [url]', 'Remote JSON Schema URL')
+  .option('-s, --schema [url]', 'Remote JSON Schema URL')
   .example((name) => {
     return `${name} "package.json" -s "https://json.schemastore.org/package"`
   })
@@ -15,11 +16,22 @@ cli
   .example((name) => {
     return `${name} "**/*.json" -s "https://json.schemastore.org/package"`
   })
-  .action(async (src, options) => {
-    await validate({
-      src: src,
-      schema: options.schema,
-    })
+  .action(async (src: string[], options) => {
+    try {
+      if (src.length === 0) throw new Error('src is required')
+      if (options.s == undefined || options.schema == undefined) throw new Error('schema option is required')
+
+      if (typeof options.s !== 'string' || typeof options.schema !== 'string') {
+        throw new Error('schema option is string only')
+      }
+
+      await validate({
+        src: src,
+        schema: options.schema,
+      })
+    } catch (error) {
+      consola.error(error)
+    }
   })
 
 cli.version('0.1.0')
