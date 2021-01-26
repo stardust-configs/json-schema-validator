3ae7501d3ad80da18c55cda6cb6aeee1154ee364
P-Chan <hello@0x50.io>
2021-01-27 00:36:38 +0900

    test: Add local schema file test

--- test/validate.test.ts
+++ test/validate.test.ts
@@ -1,4 +1,21 @@
 import { validate } from '../src/validate'
+import fetch from 'node-fetch'
+import fs from 'fs'
+import path from 'path'
+
+beforeAll(async () => {
+  const cwd = process.cwd()
+
+  const schema = await fetch('https://json.schemastore.org/tsconfig').then(async (data) => {
+    return await data.json()
+  })
+
+  const schemaFullPath = path.resolve(cwd, './examples/tsconfig.schema.json')
+
+  await fs.promises.writeFile(schemaFullPath, JSON.stringify(schema), { encoding: 'utf-8' })
+
+  return
+})
 
 test('valid', async () => {
   const result = await validate({
@@ -41,6 +58,24 @@ test('schema does not exist', async () => {
     validate({
       src: ['./examples/tsconfig.es2020.json'],
       schema: 'https://json.schemastore.org/typoscriptconfig',
+    })
+  ).rejects.toThrow()
+})
+
+test('local schema file', async () => {
+  const result = await validate({
+    src: './examples/tsconfig.es2020.json',
+    schema: './examples/tsconfig.schema.json',
+  })
+
+  expect(result).toBe(undefined)
+})
+
+test('local schema file does no exist', async () => {
+  await expect(
+    validate({
+      src: './examples/tsconfig.es2020.json',
+      schema: './examples/typoconfig.schema.json',
     })
   ).rejects.toThrow()
 })
